97f406edd3c7db2891ed65d2e6f2a98a69f84473
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-09-03 16:51:15 -0500

    Read users

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -1,22 +1,15 @@
 'use strict';
 
-// Register `phoneList` component, along with its associated controller and template
 angular.
   module('angularjsCrud').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
-    controller: function UserListController() {
-      this.users = [
-        {
-          name: 'Joao',
-          country: 'Brasil'
-        }, {
-          name: 'Jose',
-          country: 'Mexico'
-        }, {
-          name: 'David',
-          country: 'Canada'
-        }
-      ];
+    controller: function UserListController($http) {
+      var self = this;
+      self.orderProp = 'age';
+
+      $http.get('users/users.json').then(function(response) {
+        self.users = response.data;
+      });
     }
   });

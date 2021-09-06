2b7b3464156f094526c55f175bb0356a760d4cdc
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-09-06 04:45:37 -0500

    refactor

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -5,32 +5,27 @@ angular.
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
     controller: function UserListController($http) {
-      var self = this;
-
-      $http.get('users/users.json').then(function (response) {
-        self.users = response.data;
+      $http.get('users/users.json').then(response => {
+        this.users = response.data;
       });
-
-      self.createUser = (user) => {
-        console.log(user)
-        self.users.push({ name: self.name, country: self.country });
-        self.name = '';
-        self.country = '';
+      this.createUser = user => {
+        if (this.name && this.country) {
+          this.users.push({ name: this.name, country: this.country });
+          this.name = '';
+          this.country = '';
+        }
       };
-
-      self.selectUser=(user)=>{
-        console.log(user)
-        self.name = user.name;
-        self.country = user.country;
+      this.updateUser = user => {
+        if (this.name && this.country) {
+          this.users.splice(this.users.indexOf(user), 1, { name: this.name, country: this.country });
+          this.name = '';
+          this.country = '';
+        } else {
+          alert('You must fill the input fields with the new information');
+        }
       };
-
-      self.updateUser=(user)=>{
-         self.users.splice(self.users.indexOf(user), 1, { name: self.name, country: self.country });
-      };
-
-      self.deleteUser=user=>{
-        console.log(self.users.indexOf(user))
-        self.users.splice(self.users.indexOf(user),1)
+      this.deleteUser = user => {
+        this.users.splice(this.users.indexOf(user), 1)
       }
     }
   });
